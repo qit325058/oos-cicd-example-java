261fed41e94a759576409e314e4cdcbfb33eac3f
Colin Yin <yinronggang.yrg@alibaba-inc.com>
2024-06-11 15:33:08 +0800

    add

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,11 @@
-package com.example.demo2;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-public class Demo2Application {
+public class DemoApplication {
 
 	@RequestMapping("/hello")
 	public String hello() {
@@ -13,7 +13,7 @@ public class Demo2Application {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demo2Application.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
